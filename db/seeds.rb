8ff98a144dfba723c42c9bdb88bc2f52a5ffdda1
Guillaume Teillais <guillaume.teillais@gmail.com>
2022-09-21 18:05:01 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,20 @@ User.create!(email: "guillaume@test.com", password: "123456")
 
 puts "Creating lists..."
 
-List.create!(name: "Action")
-List.create!(name: "Bagare")
-List.create!(name: "Boule")
+file = URI.open("https://cdn.flickeringmyth.com/wp-content/uploads/2022/07/essential-80s-action.jpg")
+list = List.new(name: "Action")
+list.photo.attach(io: file, filename: "action.jpg", content_type: "image/jpg")
+list.save!
 
+file = URI.open("https://actuanewsmedia.files.wordpress.com/2021/03/top-10-rocky-movie-fight-scenes-kung-fu-kingdom-770x472_orig.jpg?w=1024")
+list = List.new(name: "Bagarre")
+list.photo.attach(io: file, filename: "action.jpg", content_type: "image/jpg")
+list.save!
+
+file = URI.open("https://www.eklecty-city.fr/wp-content/uploads/2020/10/american-pie-presents-girls-rules-movie-picture-01.jpg")
+list = List.new(name: "Boules")
+list.photo.attach(io: file, filename: "action.jpg", content_type: "image/jpg")
+list.save!
 
 puts "Creating movies..."
 
